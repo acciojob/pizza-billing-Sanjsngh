e0150ff565faf7027e003e0feaa955b7bfb4b78c
sanjsngh <sanjivanisngh@gmail.com>
2023-04-22 15:33:28 +0530

    Code added1

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -9,7 +9,6 @@ public class Pizza {
 
     private boolean extraCheeseAdded;
     private boolean extraToppingsAdded;
-    private boolean isBillCreated;
     private boolean takeAway;
 
     public Pizza(Boolean isVeg){
@@ -18,7 +17,6 @@ public class Pizza {
         this.toppings = this.isVeg ? 70 : 120;
         this.extraCheeseAdded = false;
         this.extraToppingsAdded = false;
-        this.isBillCreated = false;
         this.takeAway = false;
 
         this.bill = "Base Price Of The Pizza: "+this.price+"\n";
@@ -51,24 +49,22 @@ public class Pizza {
     }
 
     public String getBill(){
-        if(!isBillCreated) {
-            if(extraCheeseAdded) {
-                this.bill += "Extra Cheese Added: 80"+"\n";
-            }
-
-            if(extraToppingsAdded) {
-                this.bill += "Extra Toppings Added: " + this.toppings+"\n";
-            }
-
-            if(takeAway){
-                this.bill += "Paperbag Added: 20" + "\n";
-            }
-
-            this.bill += "Total Price: "+this.price;
-            this.isBillCreated = true;
-            return this.bill;
+        if(extraCheeseAdded) {
+            this.bill += "Extra Cheese Added: 80"+"\n";
         }
 
-        return "";
+        if(extraToppingsAdded) {
+            this.bill += "Extra Toppings Added: " + this.toppings+"\n";
+        }
+
+        if(takeAway){
+            this.bill += "Paperbag Added: 20" + "\n";
+        }
+
+        this.bill += "Total Price: "+this.price;
+        return this.bill;
+
+
+
     }
 }
